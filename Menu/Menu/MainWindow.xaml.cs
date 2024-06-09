fe33b74e642c879769f65f3846926d1cd0269bc3
preck273 <72576947+preck273@users.noreply.github.com>
2024-06-09 15:09:22 +0200

    Game Menu

---
+++ Menu/Menu/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace Menu
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+
+		private MediaPlayer mediaPlayer;
+
+		public MainWindow()
+		{
+			InitializeComponent();
+
+			mediaPlayer  = new MediaPlayer();
+
+			mediaPlayer.Open(new Uri("C:\\Users\\Pepelito\\Downloads\\Sound\\backgroundMusic.mp3", UriKind.Relative));
+			mediaPlayer.Volume = 0.5; // Set initial volume to 50%
+			mediaPlayer.MediaEnded += MediaPlayer_MediaEnded;
+			mediaPlayer.Play();
+		}
+
+
+		private void MediaPlayer_MediaEnded(object sender, EventArgs e)
+		{
+			mediaPlayer.Position = TimeSpan.Zero; // Loop the music
+			mediaPlayer.Play();
+		}
+
+		private void btnPlay_Click(object sender, RoutedEventArgs e)
+		{
+			MessageBox.Show("Play button clicked!");
+		}
+
+		private void btnSettings_Click(object sender, RoutedEventArgs e)
+		{
+			SettingsWindow settingsWindow = new SettingsWindow(mediaPlayer.Volume);
+			settingsWindow.VolumeChanged += SettingsWindow_VolumeChanged;
+			settingsWindow.ShowDialog();
+		}
+
+		private void SettingsWindow_VolumeChanged(object sender, double e)
+		{
+			mediaPlayer.Volume = e;
+		}
+
+		private void btnQuit_Click(object sender, RoutedEventArgs e)
+		{
+			Application.Current.Shutdown();
+		}
+	}
+}
---
+++ Menu/Menu/SettingsWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Menu
+{
+    /// <summary>
+    /// Interaction logic for SettingsWindow.xaml
+    /// </summary>
+    public partial class SettingsWindow : Window
+    {
+
+		private bool isMuted = false;
+
+		public event EventHandler<double> VolumeChanged;
+
+		public SettingsWindow(double currentVolume)
+        {
+            InitializeComponent();
+			sliderVolume.Value = currentVolume * 100; 
+		}
+
+		 private void sliderVolume_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            VolumeChanged?.Invoke(this, sliderVolume.Value / 100);
+
+
+		}
+
+		private void btnMute_Click(object sender, RoutedEventArgs e)
+		{
+			if (isMuted)
+			{
+				imgMute.Source = new BitmapImage(new Uri("C:\\Users\\Pepelito\\Downloads\\Image\\sound2.png", UriKind.Absolute));
+				sliderVolume.Value = 30.0; // Set to a default volume when unmuted
+			}
+			else
+			{
+				imgMute.Source = new BitmapImage(new Uri("C:\\Users\\Pepelito\\Downloads\\Image\\muteU2.png", UriKind.Absolute));
+				sliderVolume.Value = 0;
+			}
+
+			isMuted = !isMuted;
+
+
+		}
+	}
+}
