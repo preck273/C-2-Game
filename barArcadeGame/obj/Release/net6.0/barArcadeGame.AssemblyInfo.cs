371d0ae76de68dc2eb21ff55c6acd9c54e790b88
kaiseraftab <91316936+kaiseraftab@users.noreply.github.com>
2024-06-21 03:15:23 +0200

    database included

--- barArcadeGame/obj/Release/net6.0/barArcadeGame.AssemblyInfo.cs
+++ barArcadeGame/obj/Release/net6.0/barArcadeGame.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("barArcadeGame")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a348a66fe4cf03d64f77ce31cf868f697dba59ee")]
 [assembly: System.Reflection.AssemblyProductAttribute("barArcadeGame")]
